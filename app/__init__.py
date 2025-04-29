f6abbb4e50dc30ea01300cb0dc9ccb68ad4a4925
Daniel <dcook@spectrum4.ca>
2025-04-29 18:00:30 +0100

    Move Flask-Login user_loader to app/__init__.py for correct session handling

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,11 @@ db = SQLAlchemy()
 login_manager = LoginManager()
 mail = Mail()
 
+@login_manager.user_loader
+def load_user(user_id):
+    from .models import User
+    return User.query.get(int(user_id))
+
 def create_app():
     app = Flask(__name__)
     from .config import Config
--- app/auth_routes.py
+++ app/auth_routes.py
@@ -7,12 +7,6 @@ from . import db
 
 auth_bp = Blueprint('auth', __name__)
 
-from flask_login import login_manager
-
-@login_manager.user_loader
-def load_user(user_id):
-    from .models import User
-    return User.query.get(int(user_id))
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
